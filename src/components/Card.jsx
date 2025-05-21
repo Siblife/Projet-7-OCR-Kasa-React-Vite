3ee58fb6a304656a125553d769edaa5f72e09e81
Matthieu <mbazeugeauddev@gmail.com>
2025-05-21 13:44:41 +0200

    Css card logement + css titre logements

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ function Card({ title, cover }) {
   return (
     <div className="cardLogement">
       <img src={cover} alt={title} className="cardImg" />
-      <p>{title}</p>
+      <p className="cardTitle">{title}</p>
     </div>
   );
 }
