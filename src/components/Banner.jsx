cb97697b008b00b8963b3cad72c820082752f0ab
Matthieu <mbazeugeauddev@gmail.com>
2025-05-21 10:39:14 +0200

    Creation components banner + css

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,12 @@
-function Banner () {
-    return (
-        <div className="BannerImg">
-            <img src="\src\assets\banner_img.jpg" alt="" />
-            <p>Chez vous, partout et ailleurs</p>
-        </div>
-    )
+import "../styles/Banner.scss";
+
+function Banner() {
+  return (
+    <div className="BannerImg">
+      <img src="src\assets\banner_home.jpg" alt="" />
+      <p>Chez vous, 
+        partout et ailleurs</p>
+    </div>
+  );
 }
-export default Banner
+export default Banner;
