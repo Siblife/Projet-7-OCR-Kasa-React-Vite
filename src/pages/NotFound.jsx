60c548f03a0c7681f49b57a16588f3bef010e94c
Matthieu <mbazeugeauddev@gmail.com>
2025-05-28 14:31:09 +0200

    page erreur 404 composants + CSS

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,14 @@
-export default function NotFound() {
-  return <h1>404 - Page non trouvée</h1>;
+import { Link } from "react-router-dom";
+import "../styles/NotFound.scss";
+
+function NotFound() {
+  return (
+    <div className="notfound_container">
+      <h1 className="notfound_title">404</h1>
+      <p className="notfound_text">Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/" className="notfound_link">Retourner sur la page d’accueil</Link>
+    </div>
+  );
 }
+
+export default NotFound;
